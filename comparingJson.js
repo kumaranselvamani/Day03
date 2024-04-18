b57b8870ad8b4594310ab0c46a72d5b2be6badc8
kumaranselvamani <kumaranmech5@gmail.com>
2024-04-18 10:38:03 +0530

    commit

--- comparingJson.js
+++ comparingJson.js
@@ -1,4 +1,8 @@
 let obj1 = {name: "person 1", age: 5};
 let obj2 = { age: 5, name: "person 1"};
 
-console.log()
+if(obj1.name == obj2.name && obj1.age == obj2.age ){
+    console.log(true); 
+}else{
+    console.log(false); 
+}
